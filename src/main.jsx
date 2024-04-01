e462f27843d70eaa98fab1bbd6dd5c17a10ea626
[Sahil khatiwada] <sahilkhatiwada12@gmail.com>
2024-04-01 23:23:52 +0545

    Feat: project setup

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import allRoutes from "./routes";
+import { Provider } from "react-redux";
+import store from "./store";
+
+const queryClient = new QueryClient();
+const router = createBrowserRouter(allRoutes);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
